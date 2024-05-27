9cc5eac5b37ebbe6db29a599226364ed5f0aa5c5
Irfandi Iqbal Abimanyu <irfandiabimanyu@gmail.com>
2024-05-27 10:49:51 +0700

    fix: fixing login error msg

--- src/requests/login.request.js
+++ src/requests/login.request.js
@@ -7,13 +7,6 @@ export async function reqLogin(credentials) {
 	try {
 		await signIn('credentials', { ...credentials, redirect: false });
 	} catch (err) {
-		if (err instanceof AuthError) {
-			switch (err.type) {
-				case 'CredentialsSignin':
-					throw new Error('Email/Username atau Password salah!');
-				default:
-					throw new Error('Terjadi kesalahan saat masuk');
-			}
-		}
+		throw new Error('Email/Username atau Password salah!');
 	}
 }
